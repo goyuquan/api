9bace40c51daa97a70d49f5b53bc6b98bb6042de
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-28 18:38:42 +0800

    0954x3x4567hh74c

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -2,6 +2,7 @@
 
 use Faker\Generator as Faker;
 
+
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'phone' => $faker->numberBetween(10000000000, 19999999999),
@@ -10,3 +11,13 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+// $factory->define(App\Contacts::class, function (Faker $faker) {
+//     return [
+//         'phone' => $faker->numberBetween(10000000000, 19999999999),
+//         'password' => '$2y$10$sr6a1PMteAvWvGZMdmhTPu7m3G9SAj/2uBRovZ3ZiEDwsVPtxs3Vu', //123123
+//         'name' => $faker->name,
+//         'remember_token' => str_random(10),
+//     ];
+// });
