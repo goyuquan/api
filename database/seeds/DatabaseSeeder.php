d0fdc8b1f59ee2153aad42e63b0b75741b22064e
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-28 17:42:10 +0800

    oiy9t6d7dtyd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call([
-        //     UsersTableSeeder::class,
-        // ]);
+        $this->call([
+            UsersTableSeeder,
+        ]);
 
         // factory(App\User::class, 50)->create()->each(function ($u) {
         //     $u->contacts()->save(factory(App\Contacts::class)->make());
