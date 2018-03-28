df7a96e97ec7eea130a0259d6d0c817041664f0a
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-28 17:46:25 +0800

    tft

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,9 +1,9 @@
 <?php
 
-// $faker = new Faker\Generator();
-// $faker->addProvider(new Faker\Provider\en_US\PhoneNumber($faker));
+$faker = new Faker\Generator();
+$faker->addProvider(new Faker\Provider\en_US\PhoneNumber($faker));
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function () {
     return [
         'phone' => $faker->phoneNumber,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
