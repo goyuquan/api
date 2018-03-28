e7283eb39a0e3f395129a6e88261a97eaed76915
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-28 18:28:32 +0800

    hfdtu5y65687

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'phone' => $faker->numberBetween(11, 11),
+        'phone' => $faker->numberBetween(10000000000, 19999999999),
         'password' => '$2y$10$sr6a1PMteAvWvGZMdmhTPu7m3G9SAj/2uBRovZ3ZiEDwsVPtxs3Vu',
         'name' => $faker->name,
         'remember_token' => str_random(10),
