5fbb81b95e0a49d6e8a71644cdc42c98d980fb23
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-28 16:29:06 +0800

    6546846

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,6 +14,6 @@
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
-        'phone' => $faker->phone,
+        'email' => $faker->email,
     ];
 });
