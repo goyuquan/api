23c5da19ba0ba7f39841bc3c70fcac1e1bdb720f
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-28 17:54:29 +0800

    est543wq46a4srt

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,11 +1,12 @@
 <?php
 
-$faker = new Faker\Generator();
-$faker->addProvider(new Faker\Provider\en_US\PhoneNumber($faker));
+// $faker = new Faker\Generator();
+// $faker->addProvider(new Faker\Provider\en_US\PhoneNumber($faker));
+use Faker\Generator as Faker;
 
-$factory->define(User::class, function () {
+$factory->define(App\User::class, function (Faker $faker) {
     return [
-        'phone' => $faker->phoneNumber,
+        'phone' => $faker->name,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
         'name' => $faker->name,
         'remember_token' => str_random(10),
