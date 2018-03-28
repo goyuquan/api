056ca87e5b1d85c9daa46574467d6cf4d1adcf13
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-28 14:36:11 +0800

    sdafsdfdf

--- database/migrations/2018_03_27_100124_create_users_table.php
+++ database/migrations/2018_03_27_100124_create_users_table.php
@@ -15,8 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('phone');
+            $table->string('password');
             $table->string('name');
-            $table->string('email');
             $table->rememberToken();
             $table->timestamps();
         });
