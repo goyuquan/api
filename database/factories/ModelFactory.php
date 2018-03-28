eb6f27db117b7e938e2770f72a27cc18f414b861
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-28 17:37:30 +0800

    hjfj67re67e65d

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,13 +1,21 @@
 <?php
 
-$faker = new Faker\Generator();
-$faker->addProvider(new Faker\Provider\en_US\PhoneNumber($faker));
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
 
-$factory->define(App\User::class, function () {
+$factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'phone' => $fake->phoneNumber,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
-        'name' => $faker->name,
-        'remember_token' => str_random(10),
+      'phone' => $fake->name,
+      'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
+      'name' => $faker->name,
+      'remember_token' => str_random(10),
     ];
 });
