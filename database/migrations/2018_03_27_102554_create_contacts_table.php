2e710846646c5e9be2a90b82aea149a8e256e088
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-29 10:46:24 +0800

    waetsgdgd

--- database/migrations/2018_03_27_102554_create_contacts_table.php
+++ database/migrations/2018_03_27_102554_create_contacts_table.php
@@ -15,6 +15,7 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('phone');
             $table->string('name');
